0a7712503c731d70bfc1f826bb10114194b4e58b
Siarhei Kandratsiuk <siarhei_kandratsiuk@epam.com>
2022-04-05 21:11:45 +0300

    use class instance

--- test/LoanCalculator.spec.js
+++ test/LoanCalculator.spec.js
@@ -1,5 +1,8 @@
 const assert = require('assert');
 const LoanCalculator = require('../src/LoanCalculator');
+const CarLoanCalculator = require("../src/CarLoanCalculator");
+const StudentLoanCalculator = require("../src/StudentLoanCalculator");
+const HouseLoanCalculator = require("../src/HouseLoanCalculator");
 
 describe('LoanCalculator', () => {
     let calculator;
@@ -9,54 +12,54 @@ describe('LoanCalculator', () => {
     });
 
     it('student loan for young poor person', () => {
-        assert.strictEqual(calculator.getLoan('student', 18, 100), 100);
+        assert.strictEqual(calculator.getLoan(new StudentLoanCalculator(), 18, 100), 100);
     });
 
     it('student loan for old person', () => {
-        assert.strictEqual(calculator.getLoan('student', 21, 100), 250);
+        assert.strictEqual(calculator.getLoan(new StudentLoanCalculator(), 21, 100), 250);
     });
 
     it('student loan for rich person', () => {
-        assert.strictEqual(calculator.getLoan('student', 18, 2000), 200);
+        assert.strictEqual(calculator.getLoan(new StudentLoanCalculator(), 18, 2000), 200);
     });
 
     it('car loan for young poor person', () => {
-        assert.strictEqual(calculator.getLoan('car', 20, 500), 2000);
+        assert.strictEqual(calculator.getLoan(new CarLoanCalculator(), 20, 500), 2000);
     });
 
     it('car loan for adult poor person', () => {
-        assert.strictEqual(calculator.getLoan('car', 45, 500), 3000);
+        assert.strictEqual(calculator.getLoan(new CarLoanCalculator(), 45, 500), 3000);
     });
 
     it('car loan for old poor person', () => {
-        assert.strictEqual(calculator.getLoan('car', 60, 500), 3500);
+        assert.strictEqual(calculator.getLoan(new CarLoanCalculator(), 60, 500), 3500);
     });
 
     it('car loan for young rich person', () => {
-        assert.strictEqual(calculator.getLoan('car', 20, 2000), 4000);
+        assert.strictEqual(calculator.getLoan(new CarLoanCalculator(), 20, 2000), 4000);
     });
 
     it('car loan for adult rich person', () => {
-        assert.strictEqual(calculator.getLoan('car', 45, 2000), 6000);
+        assert.strictEqual(calculator.getLoan(new CarLoanCalculator(), 45, 2000), 6000);
     });
 
     it('car loan for old rich person', () => {
-        assert.strictEqual(calculator.getLoan('car', 60, 2000), 7000);
+        assert.strictEqual(calculator.getLoan(new CarLoanCalculator(), 60, 2000), 7000);
     });
 
     it('house loan for young poor person', () => {
-        assert.strictEqual(calculator.getLoan('house', 20, 500), 100000);
+        assert.strictEqual(calculator.getLoan(new HouseLoanCalculator(), 20, 500), 100000);
     });
 
     it('house loan for old poor person', () => {
-        assert.strictEqual(calculator.getLoan('house', 60, 500), 100000);
+        assert.strictEqual(calculator.getLoan(new HouseLoanCalculator(), 60, 500), 100000);
     });
 
     it('house loan for young rich person', () => {
-        assert.strictEqual(calculator.getLoan('house', 20, 65000), 200000);
+        assert.strictEqual(calculator.getLoan(new HouseLoanCalculator(), 20, 65000), 200000);
     });
 
     it('house loan for old rich person', () => {
-        assert.strictEqual(calculator.getLoan('house', 60, 65000), 400000);
+        assert.strictEqual(calculator.getLoan(new HouseLoanCalculator(), 60, 65000), 400000);
     });
 });
